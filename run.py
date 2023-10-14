b596213f7c6a7c1656851bd0ffcc89c59a66f1f3
Sai Prasath S <ss651@dgxstation.cc.gatech.edu>
2023-10-13 23:26:47 -0400

    updated finetuning, added test and val set

--- run.py
+++ run.py
@@ -5,19 +5,19 @@ import torch.nn as nn
 import bitsandbytes as bnb
 import transformers
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, logging
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from peft import LoraConfig, PeftModel
 from trl import SFTTrainer
 
 ### model ###
-model_name = "openlm-research/open_llama_3b_v2"
+model_name = "meta-llama/Llama-2-7b-chat-hf"
 new_model = "llama2_lora_lamni"
 base_model = AutoModelForCausalLM.from_pretrained(
     model_name,
     low_cpu_mem_usage=True,
     return_dict=True,
     torch_dtype=torch.float16,
-    device_map={"": 0}
+    device_map="auto"
 )
 model = PeftModel.from_pretrained(base_model, new_model)
 model = model.merge_and_unload()
@@ -26,9 +26,61 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+
+def convert2inputformat(text):
+    return """<s>[INST] <<SYS>>
+                        You are a domain adapted chatbot learning new information about the Lamini organization and its products. Answer the question based on your knowledge of Lamini, and do not answer questions that you are not confident about. 
+                        <</SYS>>
+
+                        ### Question: {} [/INST]""".format(text)
+
+
+dataset_name = "lamini/lamini_docs"
+test_dataset = load_dataset(dataset_name, split="test")
+
+n = len(test_dataset)
+test_dataset = test_dataset.shuffle(seed=42).select(range(int(0.30 * n), n))
+
+# questions = {'text': list(map(convert2inputformat, test_dataset['question']))}
+# questions = Dataset.from_dict(questions)
+# print(questions)
+questions = list(map(convert2inputformat, test_dataset['question']))
+
+generated_output = pipe(questions, batch_size=8, return_full_text = False, max_new_tokens=200, num_beams=3)
+
+print(generated_output)
+generated_answers = []
+for x in generated_output:
+    if 'Answer:' in x[0]['generated_text']:
+        clean_output = x[0]['generated_text'].split('Answer:')[1].strip()
+    else:
+        clean_output = x[0]['generated_text']
+        
+    if '###' in clean_output:
+        clean_output = clean_output.split('###')[0].strip()
+    generated_answers.append(clean_output)
+
+# generated_answers = [x[0]['generated_text'].split('Answer:')[1].strip() for x in generated_answers]
+
+import csv
+
+# Open a file for writing
+with open('output.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    
+    # Write the header
+    writer.writerow(["Question", "Actual Answer", "Generated Answer"])
+    
+    # Write the content
+    for q, actual, generated in zip(test_dataset['question'], test_dataset['answer'], generated_answers):
+        writer.writerow([q, actual, generated])
+
+
 ### test model ###
-logging.set_verbosity(logging.CRITICAL)
-prompt = "Who is the founder of lamini" 
-pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=100)
-result = pipe(f"### Question: {prompt}")
-print(result[0]['generated_text'])
+# logging.set_verbosity(logging.CRITICAL)
+# while 1:
+#     prompt = input("Enter the prompt: ")
+#     prompt = convert2inputformat(prompt)
+#     result = pipe(prompt, return_full_text = False, max_new_tokens=1000, num_beams=3)
+#     print(result[0]['generated_text'])
--- train.py
+++ train.py
@@ -1,33 +1,62 @@
 import os, torch, logging
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import torch
 import torch.nn as nn
 import bitsandbytes as bnb
-import transformers
-from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, EarlyStoppingCallback
 from datasets import load_dataset
 from peft import LoraConfig, PeftModel
 from trl import SFTTrainer
 
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    logging.info(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
 ### building dataset to the format: 'text': ### Question: <question> ### Answer: <answer> ###
 # reformat the dataset.
 def build_text(example):
-    example["text"] = "### question: " + str(example["question"])+ "\n### answer: " +str(example["answer"])
+    example["text"] = """<s>[INST] <<SYS>>
+                        You are a domain adapted chatbot learning new information about the Lamini organization and its products. Answer the question based on your knowledge of Lamini, and do not answer questions that you are not confident about. 
+                        <</SYS>>
+
+                        ### Question: {} [/INST] \n### Answer: {} </s>""".format(example["question"], example["answer"])
     return example
 
 dataset_name = "lamini/lamini_docs"
-dataset = load_dataset(dataset_name, split="train")
-dataset = dataset.remove_columns(["input_ids", "attention_mask", "labels"])
-dataset = dataset.map(build_text)
-dataset = dataset.remove_columns(["question", "answer"])
-print(dataset[0])
+datasets = load_dataset(dataset_name)
+
+for t in datasets:
+    datasets[t] = datasets[t].remove_columns(["input_ids", "attention_mask", "labels"])
+    datasets[t] = datasets[t].map(build_text)
+    datasets[t] = datasets[t].remove_columns(["question", "answer"])
+
+train_dataset = datasets['train']
+
+n = len(datasets['test'])
+val_dataset = datasets['test'].shuffle(seed=42).select(range(0, int(0.30 * n)))
+test_dataset = datasets['test'].shuffle(seed=42).select(range(int(0.30 * n), n))
+
+print("***** Loaded Datasets *****")
+print(train_dataset[0])
 print("==" * 50)
 # dataset = dataset.map(lambda x: tokenizer(x["text"]), batched=True)
 # print(dataset)
 
 
 ### build the model ###
-model_name = "openlm-research/open_llama_3b_v2"
+model_name = "meta-llama/Llama-2-7b-chat-hf"
 new_model = "llama2_lora_lamni"
 compute_dtype = getattr(torch, "float16")
 bnb_config = BitsAndBytesConfig(
@@ -47,20 +76,22 @@ if compute_dtype == torch.float16:
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     quantization_config=bnb_config,
-    device_map={"": 0}
+    device_map="auto"
 )
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+
+print("***** Loaded Models and Tokenizer *****")
 
 ### lora config ###
 peft_config = LoraConfig(
     r=64, #attention heads
     lora_alpha=16, #alpha scaling
-    # target_modules=["q_proj", "v_proj"], #if you know the 
+    target_modules=['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj','lm_head'], #if you know the 
     lora_dropout=0.1,
     bias="none",
     task_type="CAUSAL_LM" # set this for CLM or Seq2Seq
@@ -68,37 +99,43 @@ peft_config = LoraConfig(
 
 
 # ### Trainer ###
+
+training_arguments = TrainingArguments(
+        output_dir="./results",
+        save_strategy="steps",
+        save_steps=50,
+        save_total_limit=1,
+        evaluation_strategy="steps",
+        load_best_model_at_end=True,
+        num_train_epochs=4,
+        max_steps = 2000,
+        per_device_train_batch_size=16,
+        gradient_accumulation_steps=1,
+        optim="paged_adamw_32bit",
+        logging_steps=10,
+        learning_rate=5e-5,
+        weight_decay=0.001,
+        max_grad_norm=0.3,
+        warmup_ratio=0.03,
+        group_by_length=True,
+        lr_scheduler_type="linear",
+    )
+
 trainer = SFTTrainer(
     model=model, 
-    train_dataset=dataset,
-    args=transformers.TrainingArguments(
-        output_dir='./results',
-        num_train_epochs = 2,
-        per_device_train_batch_size=4, 
-        gradient_accumulation_steps=1,
-        optim = "paged_adamw_32bit",
-        warmup_ratio = 0.03,
-        save_steps = 0,
-        logging_steps=25, 
-        gradient_checkpointing = True,
-        weight_decay = 0.001,
-        group_by_length = True,
-        max_grad_norm = 0.3,
-        max_steps=-1, 
-        learning_rate=2e-4, 
-        fp16 = False,
-        bf16 = False,
-        report_to="none",
-        lr_scheduler_type="cosine",
-    ),
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    args=training_arguments,
     peft_config=peft_config,
     dataset_text_field="text",
-    max_seq_length=None,
     tokenizer=tokenizer,
-    packing=False
+    callbacks=[EarlyStoppingCallback(5, 0.0005)],
 )
+
+print("***** Begin Finetuning... *****")
+print_trainable_parameters(trainer.model)
 
 trainer.train()
 trainer.model.save_pretrained(new_model)
 
-print("alles gut")
+print("***** Finetuning Done *****")
