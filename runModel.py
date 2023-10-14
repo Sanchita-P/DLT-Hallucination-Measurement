aea9287e6ce741ab94e6260c38c674f568d7b1dd
Sai Prasath S <ss651@dgxstation.cc.gatech.edu>
2023-10-14 19:32:23 -0400

    cahnged file names, added the run file

---
+++ runModel.py
@@ -0,0 +1,44 @@
+import os, torch, logging
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+import transformers
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, logging
+from datasets import load_dataset, Dataset
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+### model ###
+model_name = "meta-llama/Llama-2-7b-chat-hf"
+new_model = "llama2_lora_lamni"
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map="auto"
+)
+model = PeftModel.from_pretrained(base_model, new_model)
+model = model.merge_and_unload()
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+
+def convert2inputformat(text):
+    return """<s>[INST] <<SYS>>
+                        You are a domain adapted chatbot learning new information about the Lamini organization and its products. Answer the question based on your knowledge of Lamini, and do not answer questions that you are not confident about. 
+                        <</SYS>>
+
+                        ### Question: {} [/INST]""".format(text)
+
+
+### test model ###
+logging.set_verbosity(logging.CRITICAL)
+while 1:
+    prompt = input("Enter the prompt: ")
+    prompt = convert2inputformat(prompt)
+    result = pipe(prompt, return_full_text = False, max_new_tokens=100, num_beams=3)
+    print(result[0]['generated_text'])
